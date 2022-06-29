82069ed68c70b0e958eff3a40b52a075e1281e4c
Marco Viana <marcoviana100@gmail.com>
2022-06-29 19:21:22 -0300

    Etapa 13 finalizada

--- src/challenges2.js
+++ src/challenges2.js
@@ -4,15 +4,22 @@ console.log(generatePhoneNumber(array));
 function generatePhoneNumber(numeroTel) {
   // seu código aqui
   let numeroRepetido = 0;
-  if (numeroTel.length < 11 || numeroTel.length > 11) {
-    return 'Array com tamanho incorreto.';
-  }
+  let numeroCompleto;
+
   for (let index = 0; index < numeroTel.length; index++) {
+    if (numeroTel.length < 11 || numeroTel.length > 11) {
+      return 'Array com tamanho incorreto.';
+    }
+
     if (numeroTel[index] < 0 || numeroTel[index] > 9) {
       return 'não é possível gerar um número de telefone com esses valores';
     }
+    if (numeroTel[index] >= 0 || numeroTel[index] <= 9) {
+      numeroCompleto.push(numeroTel[index]);
+    }
 
   }
+  return numeroCompleto;
 }
 
 // Desafio 12
@@ -20,7 +27,7 @@ console.log(triangleCheck(10, 14, 8));
 function triangleCheck(lineA, lineB, lineC) {
   // seu código aqui
   let result;
-  if ((lineB + lineC) < lineA ||  (lineA + lineC) < lineB || (lineA + lineB) < lineC) {
+  if ((lineB + lineC) < lineA || (lineA + lineC) < lineB || (lineA + lineB) < lineC) {
     result = false;
   }
 
@@ -28,16 +35,29 @@ function triangleCheck(lineA, lineB, lineC) {
     result = false;
   }
 
-  else
-  {
+  else {
     result = true;
   }
   return result;
 }
 
 // Desafio 13
-function hydrate() {
+console.log(hydrate('1 tequila'));
+function hydrate(string) {
   // seu código aqui
+  let contador = 0;
+for (const iterador in string) {
+  if (string[iterador] >= 1 || string[iterador] <= 9) {
+    contador = contador + Math.floor(string[iterador]);
+  }
+ 
+  
+}
+if (contador === 1) {
+  return contador + ' copo de água';
+}
+
+  return contador + ' copos de água';
 }
 
 module.exports = {
